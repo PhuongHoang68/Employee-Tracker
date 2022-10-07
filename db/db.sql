b15dc0450564e735b5dfaa10e356263f6d7cdd45
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-10-07 16:50:51 -0500

    initial files

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE team;
+
+USE team;
---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id: INTEGER
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+);
